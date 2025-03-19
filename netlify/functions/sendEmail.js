4be153af69b9c8cd305322ccd61a828ece9bce64
denslaw908 <denslaw908@outlook.com>
2025-03-19 19:44:09 +0100

    update SMTP server

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -26,7 +26,7 @@ exports.handler = async (event) => {
 
         let mailOptions = {
             from: process.env.SMTP_USER,
-            to: "amandatayloro1070@outlook.com",
+            to: "amandataylor1070@outlook.com",
             subject: `New Contact Form Submission from ${message}`,
             text: `You have received a new message from ${message}`,
         };
